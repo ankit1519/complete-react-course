884fc8b48221ac81d51f1abd06354aed8e6bcedd
ankit1519 <raazankit1502@gmail.com>
2023-10-12 14:27:38 +0530

    counter project useState react hook

--- 02_counterProject/src/App.jsx
+++ 02_counterProject/src/App.jsx
@@ -1,14 +1,24 @@
 
-
-
+import {useState} from 'react'
 import './App.css'
 
 function App() {
- 
-
+  let [counter,setCounter]=useState(1)
+  
+  const addC=function(){
+    setCounter(counter+1)
+    
+  }
+  const removeC=function(){
+    setCounter(counter-1)
+    
+  }
   return (
     <>
-      <h1>Hi</h1>
+     <h1>Counter Project</h1>
+     <h2>Count: {counter}</h2>
+     <button onClick={addC}>Add</button>
+     <button onClick={removeC}>remove</button>
     </>
   )
 }
