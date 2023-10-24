bb0d61ea03b8f818f447b9615d3076940ba5fb25
ankit1519 <raazankit1502@gmail.com>
2023-10-25 05:16:31 +0530

    added profile and login page

--- 08_miniContext/src/App.jsx
+++ 08_miniContext/src/App.jsx
@@ -1,16 +1,14 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import UserContextProvider from './context/UserContextProvider'
 
 function App() {
   
 
   return (
-    <>
-      
-       ankit Raj
-    </>
+    <UserContextProvider>
+      <h1>React with chai</h1>
+    </UserContextProvider>
   )
 }
 
